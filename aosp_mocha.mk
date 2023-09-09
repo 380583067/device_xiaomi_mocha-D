25f1ea1dfc93e86129dcc2923573c8907ce391a3
Dargons10 <jdamr2p@gmail.com>
2023-09-09 22:55:37 +0200

    Pixelify

--- aosp_mocha.mk
+++ aosp_mocha.mk
@@ -1,10 +1,10 @@
 # Inherit device configuration for mocha.
 $(call inherit-product, device/xiaomi/mocha/full_mocha.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+# Inherit some common aosp stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
 
-PRODUCT_NAME := lineage_mocha
+PRODUCT_NAME := aosp_mocha
 PRODUCT_DEVICE := mocha
 PRODUCT_BAND := xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
