3a6353bd70e438affd4c6a8dd6851ea978e7875c
apple <105837597+380583067@users.noreply.github.com>
2023-12-10 21:11:05 -0800

    Create Android.mk

---
+++ media/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := enctune.conf
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := enctune.conf
+LOCAL_MODULE_PATH  := $(TARGET_ETC_OUT)
+include $(BUILD_PREBUILT)
